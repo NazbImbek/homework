8c6cbac12e43efb282f794deb443847cd00261ef
Kimbaev <kimbaev85@gmail.com>
2021-11-07 00:47:12 +0600

    task.js is modified

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
+
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  const D = b**2 - 4 * a * c;
+    if (D > 0) {
+      arr [0]= (- b + Math.sqrt(D ))/(2 * a) 
+      arr [1] = (- b - Math.sqrt(D ))/(2 * a)
+    } else if (D === 0) {
+      arr [0] = - b / (2 * a)
+    }  else if (D < 0){
+      arr;
+    }
+    return arr; // array
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
