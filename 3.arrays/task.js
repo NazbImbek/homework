d3c6042b45e3a95214727f572f5e376febdd1152
Kimbaev <kimbaev85@gmail.com>
2021-12-13 21:50:43 +0600

    initial

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
+// 1-e
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = (arr1.length === arr2.length) && (arr1.every((val,idx) => val === arr2[idx]));
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
-
+// 2-e
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(val => val > 0).filter(val => val % 3 === 0).map(val => val * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr; 
 }
