2e04cca5bccdc8f1bdf69014994111500526256d
Kimbaev <kimbaev85@gmail.com>
2021-12-17 18:12:49 +0600

    the error fixed

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -7,13 +7,12 @@ class PrintEditionItem {
     this.state = 100;
     this.type = null;
     }
-}
  
-fix() {
+  fix() {
     return this.state *= 1.5;
-}
+  }
 
-set state(number) {
+  set state(number) {
     this.state = number;
     if (number < 0) {
       this._state = 0; 
@@ -22,9 +21,10 @@ set state(number) {
     } else {
       this._state = this.state;
     }
-}
-get state() {
+  }
+  get state() {
     return this._state;
+  }
 }
    
 class Magazine extends PrintEditionItem {
@@ -70,28 +70,29 @@ class Library {
     this.name = name;
     this.books = [];
   }
-}
 
-addBook(book) {
+
+  addBook(book) {
   if (this.state > 30) {
     return this.books.push(book);
     }
-}
+  }
 
-findBookBy(type, value) {
-  for(let i = 0; i < this.books.length; i++) {
-    if(this.books[i][type] === value) {
+  findBookBy(type, value) {
+    for(let i = 0; i < this.books.length; i++) {
+      if(this.books[i][type] === value) {
         return this.books[i];
-    } 
-  }
-    return null;
-}
-
-giveBookByName(bookName) {
-  for(let i = 0; i < this.books.length; i++) {
-    if (this.books[i].name === bookName) {
-     return  this.books.splice(i, 1)[0];
+      } 
     }
-  }
     return null;
+  }
+
+  giveBookByName(bookName) {
+    for(let i = 0; i < this.books.length; i++) {
+      if (this.books[i].name === bookName) {
+        return  this.books.splice(i, 1)[0];
+      }
+    }
+    return null;
+  }
 }
